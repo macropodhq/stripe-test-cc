9c60520045738bd15ec13ef4e6a9100c98b7d841
Alan Harper <alan@aussiegeek.net>
2015-07-23 09:43:10 +1000

    Make popup close after a card is selected

--- popup.js
+++ popup.js
@@ -36,6 +36,7 @@ document.addEventListener('DOMContentLoaded', function() {
       chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, card);
       });
+      window.close();
     }
     li.appendChild(document.createTextNode(card.name));
     li.appendChild(document.createElement('br'));
